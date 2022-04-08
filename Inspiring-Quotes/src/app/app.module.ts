13419cd87422e41aa5a5c1f21a652b3abfdc8a93
Collins Sirwani <sircollo71@gmail.com>
2022-04-09 01:31:39 +0300

    import forms module from angular forms

--- Inspiring-Quotes/src/app/app.module.ts
+++ Inspiring-Quotes/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { QuotesDetailsComponent } from './quotes-details/quotes-details.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
