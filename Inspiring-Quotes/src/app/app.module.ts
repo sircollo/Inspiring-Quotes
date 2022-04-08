acfda5a1fa9e047861e21a378d48984320a0b524
Collins Sirwani <sircollo71@gmail.com>
2022-04-08 20:48:55 +0300

    Feat: add quotes and component and quotes class

--- Inspiring-Quotes/src/app/app.module.ts
+++ Inspiring-Quotes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SliderComponent } from './slider/slider.component'
+import { SliderComponent } from './slider/slider.component';
+import { QuotesComponent } from './quotes/quotes.component'
 @NgModule({
   declarations: [
     AppComponent,
-    SliderComponent
+    SliderComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
