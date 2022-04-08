d5dbbe360faf27da99d25bc3fdf458cc5de5926c
Collins Sirwani <sircollo71@gmail.com>
2022-04-08 19:21:00 +0300

    Import slider component

--- Inspiring-Quotes/src/app/app.module.ts
+++ Inspiring-Quotes/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { SliderComponent } from './slider/slider.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
