7502b3c31928de694358ba5d0c65469332a66128
Collins Sirwani <sircollo71@gmail.com>
2022-04-08 17:58:56 +0300

    fix: edit images array and increase image change interval

--- Inspiring-Quotes/src/app/app.component.ts
+++ Inspiring-Quotes/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class AppComponent {
   title = 'Inspiring-Quotes';
+  images = [
+    {title: 'Wangari Maathai',src: "assets/mathai.jpeg"},
+    {title: 'Bill Gates',src: "assets/bill.jpeg"},
+    {title: 'Steve Jobs',src: "assets/steve.jpeg"}
+  ];
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 3000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 }
