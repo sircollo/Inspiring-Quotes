9fc86a89be2adf3669ba2edab5348b4c68f5a06c
Collins Sirwani <sircollo71@gmail.com>
2022-04-08 21:32:32 +0300

    Feat: adds form component for collecting user inputs

--- Inspiring-Quotes/src/app/app.module.ts
+++ Inspiring-Quotes/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SliderComponent } from './slider/slider.component';
-import { QuotesComponent } from './quotes/quotes.component'
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component'
 @NgModule({
   declarations: [
     AppComponent,
     SliderComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
