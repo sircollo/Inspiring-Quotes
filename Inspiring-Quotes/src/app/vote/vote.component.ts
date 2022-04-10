6724dbbe4194192d616c0db4c955498a45e17793
Collins Sirwani <sircollo71@gmail.com>
2022-04-10 16:21:00 +0300

    Feat: adds up and down vote buttons and give logic to button click events

--- Inspiring-Quotes/src/app/vote/vote.component.ts
+++ Inspiring-Quotes/src/app/vote/vote.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vote.component.css']
 })
 export class VoteComponent implements OnInit {
+  upVotes = 0;
+  downVotes = 0;
+
+  upVote(){
+    this.upVotes = this.upVotes + 1;//adds one on mouse click
+  }
+
+  downVote(){
+    this.downVotes = this.downVotes + 1;//adds one per mouse click
+  }
 
   constructor() { }
 
